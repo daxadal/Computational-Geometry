16db798b690029b42a348a2e68dc74e1a942d442
mendi0 <jmendi01@ucm.es>
2017-05-02 23:08:34 +0200

    net_constructor updated and maybe functional.

--- Practica_4/net_constructor.py
+++ Practica_4/net_constructor.py
@@ -3,94 +3,95 @@ from __future__ import print_function, division
 import tensorflow as tf
 import numpy as np
 import sys
+from datetime import datetime
 
-def NetConstructor(object):
+NOW = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+ROOT_LOGDIR = 'tf_logs'
+LOG_DIR = "{}/run-{}".format(ROOT_LOGDIR, NOW)
+
+class NetConstructor(object):
     def __init__(self, layers):
-        '''
-        layers es una lista de diccionarios que describirán las sucesivas capas de la red.
-        layers = [layer_0, layer_1, layer_2, layer_3, ..., layer_m]
-        layer_0 contendrá solamente la dimensión de los datos de entrada, que será una tupla o un número:
-        Por ejemplo, layer_0 = {'dim': (224, 224, 3)} en el caso de que los 
-        datos de entrada sean imágenes de dimensión 224 x 224 y 3 canales de color o
-        layer_0 = {'dim': 784} en el caso de que sean vectores que representen imágenes de MNIST.
-        En las restantes capas, la estructura será la siguiente:
-        (se indican los parámetros mínimos que deberán estar implementados, se
-         pueden añadir otros si se desea.)
-        layer_k = {'type': layer_type, # tipo de capa: 'fc', 'conv', 'maxpool', 'dropout', 'LRN', ...
-                   'dim': (dim_0, ..., dim_L) # dimensiones de la salida
-                                              # de la capa (en su caso)
-                   'kernel_size': size # por ejemplo, (3, 3) en una máscara convolucional 3 x 3
-                   'stride': stride # por ejemplo, (1, 1) si se hace stride 1 horizontal y 1 vertical
-                   'init': init_method # método de inicialización de pesos y biases, por ejemplo
-                                       # ('truncated_normal', stddev, 'zeros'), 'xavier' o 'he'
-                   'padding': padding # 'SAME', 'VALID'
-                   'activation': activation, # función de activación, 
-                                             # 'sigmoid', 'tanh', 'relu', 'identity', ...
-                   'prob': probability, # float, probabilidad usada en dropout
-                   'LRN_params': (k, alpha, beta, r)}
-        '''
         tf.reset_default_graph()
-        
-        '''self.init_dict = {'truncated_normal': tf.truncated_normal,
-                          'xavier': tf.layers.xavier_initializer,
-                          'he': }'''     
         
         self.loss_dict = {'softmax': tf.nn.softmax_cross_entropy_with_logits,
                           'identity': tf.nn.l2_loss,
                           'sigmoid': tf.nn.sigmoid_cross_entropy_with_logits}
+        for i in range(len(layers)):
+            if type(layers[i]['dim']) is int:
+                layers[i]['dim'] = (layers[i]['dim'],)
+            if not ('init' in layers[i]):
+                layers[i]['init'] = 'truncated_normal'
         self.layers = layers
         self.create_net()
     
     def create_net(self):
-        def index(self, dim):
+        def index(dim):
             if type(dim) is tuple: index = len(dim)-1
             else: index = 0
             return index
+
+        def reduce_mul(l):
+            ret = 1
+            for i in range(len(l)):
+                ret *= l[i]
+            return ret
         
-        def fc_layer(self, unit, dim, params):
+        def fc_layer(unit, dim, params):
+            '''
             if index(dim) == 2: newdim = dim[0] * dim[1] * dim[2]
             elif index(dim) == 1: newdim = dim[0] * dim[1]
             else: newdim = dim
-            
-            activate = activations_dict[params['activation']]
-            #init = self.init_dict[params['init']]
-            init = tf.truncated_normal(weights_shape)
+            '''
+
+            h = activations_dict[params['activation']]
+            flat_dim = reduce_mul(dim)
+            #final_dim = list((int(unit.get_shape()[0]),) + dim)
+            unit_dim = [-1] + [reduce_mul(unit.get_shape().as_list()[1:])]
+            final_dim = list((-1,)+dim)
+            #
+            init = self.init_dict[params['init']]
+            #init = tf.truncated_normal(weights_shape)
             with tf.name_scope('fc_layer'):
-                weights_shape = (int(unit.get_shape()[1]), newdim)
-                weights = tf.Variable(init, name='weights')
-                bias = tf.Variable(tf.zeros(newdim), name='bias')
-                activ = tf.add(tf.matmul(unit, weights), bias, name='activation')
-                return activate(activ, name='unit')
+                reshaped_unit = tf.reshape(unit, unit_dim)
+                weights_shape = (unit_dim[1], flat_dim)
+                #weights = tf.Variable(tf.truncated_normal(weights_shape), name='weights')
+                weights = tf.Variable(init(weights_shape), name='weights')
+                bias = tf.Variable(tf.zeros(flat_dim), name='bias')
+                activ = tf.add(tf.matmul(reshaped_unit, weights), bias, name='activation')
+                return h(tf.reshape(activ, final_dim), name='unit')
         
-        def conv_layer(self, unit, dim, params):
+        def conv_layer(unit, dim, params):
             activation = activations_dict[params['activation']]
             #init = self.init_dict[params['init']]
-            init = tf.layers.xavier_initializer
+            init = tf.contrib.layers.xavier_initializer()
             layer_list = [tf.layers.conv1d, tf.layers.conv2d, tf.layers.conv3d]
-            return layer_list[index(dim)](inputs=unit,
-                                    filters=dim,
-                                    kernel_size=params['kernel_size'],
-                                    strides=params['stride'],
-                                    padding=params['padding'],
-                                    activation=activation,
-                                    kernel_initializer=init,
-                                    bias_initializer=init,
-                                    name='conv_layer')
+            #index(dim) o len(params['kernel_size'])
+            conv_dim = len(params['kernel_size'])
+            return layer_list[conv_dim](inputs=unit,
+                                        filters=dim,
+                                        kernel_size=params['kernel_size'],
+                                        strides=params['stride'],
+                                        padding=params['padding'],
+                                        activation=activation,
+                                        kernel_initializer=init,
+                                        bias_initializer=init,
+                                        name='conv_layer')
         
-        def maxpool_layer(self, unit, dim, params):
+        def maxpool_layer(unit, dim, params):
             layer_list = [tf.layers.max_pooling1d, tf.layers.max_pooling2d, tf.layers.max_pooling3d]
-            return layer_list[index(dim)](inputs=unit,
+            conv_dim = len(params['kernel_size'])
+            return layer_list[conv_dim](inputs=unit,
                                     pool_size=params['kernel_size'],
                                     strides=params['stride'],
                                     padding=params['padding'],
                                     name='maxpool_layer')
                                     
-        def dropout_layer(self, unit, dim, params):
+        def dropout_layer(unit, dim, params):
             return tf.layers.dropout(inputs=unit,
                                      rate=params['prob'],
                                      name='dropout_layer')
         
-        def LRN_layer(self, unit, dim, params):
+        def LRN_layer(unit, dim, params):
             return tf.nn.local_response_normalization(
                               inputs=unit,
                               bias=params['LRN_params'][0],
@@ -100,46 +101,37 @@ def NetConstructor(object):
                               name='LRN_layer')
                               
         layer_dict = {'fc': fc_layer,
-                           'conv': conv_layer,
-                           'maxpool': maxpool_layer,
-                           'dropout': dropout_layer,
-                           'LRN': LRN_layer}
+                      'conv': conv_layer,
+                      'maxpool': maxpool_layer,
+                      'dropout': dropout_layer,
+                      'LRN': LRN_layer}
         activations_dict = {'relu': tf.nn.relu,
-                                 'sigmoid': tf.nn.sigmoid,
-                                 'tanh': tf.nn.tanh,
-                                 'identity': tf.identity}
+                            'sigmoid': tf.nn.sigmoid,
+                            'tanh': tf.nn.tanh,
+                            'identity': tf.identity,
+                            'softmax': tf.nn.softmax}
+        self.init_dict = {'truncated_normal': tf.truncated_normal_initializer(),
+                          'xavier': tf.contrib.layers.xavier_initializer(),
+                          'he': tf.contrib.keras.initializers.he_normal()} # or uniform?
+        
                                 
-        self.x = tf.placeholder(tf.float32, shape= self.layers[0]['dim'], name='x')
-        self.y_ = tf.placeholder(tf.float32, shape=self.layers[-1]['dim'], name='y_')
+        self.x = tf.placeholder(tf.float32, shape=(None,)+self.layers[0]['dim'], name='x')
+        self.y_ = tf.placeholder(tf.float32, shape=(None,)+self.layers[-1]['dim'], name='y_')
         unit = self.x;
-        for type, dim, params in zip(self.layers[1: -1]['type'],
-                                     self.layers[1: -1]['dim'],
-                                     self.layers[1: -1]):
-            layer = layer_dict[type]
-            unit = layer(unit, dim, params)
+        for layer in self.layers[1:-1]:
+            layer_type = layer_dict[layer['type']]
+            unit = layer_type(unit, layer['dim'], layer)
             
-        layer = layer_dict(self.layers[-1]['type'])
-        self.y = layer(unit, self.layers[-1]['dim'], self.layers[-1]) #la ultima capa debe tener activación identity
+        layer_type = layer_dict[self.layers[-1]['type']]
+        self.last_activation = self.layers[-1]['activation']
+        self.layers[-1]['activation'] = 'identity'
+        self.logits = layer_type(unit, self.layers[-1]['dim'], self.layers[-1]) #la ultima capa debe tener activacion identity
     
     def train(self, x_train, t_train,
               nb_epochs=1000,
               batch_size=10,
               method=('SGD', {'eta': 0.1}),
               seed=tf.set_random_seed(1)):
-        '''
-        El método train entrenará la red, recibiendo los datos de entrenamiento,
-        número de epochs, tamaño del batch, una semilla opcional para el
-        generador de números aleatorios y el método de entrenamiento:
-
-        method = (str, params),
-
-        el primer elemento describe el método de optimización,
-        por ejemplo, 'SGD', 'nesterov', 'momentum', 'adagrad', 'adadelta', 'RMSprop'.
-        El segundo elemento es un diccionario de parámetros adaptados al método,
-        siguiendo la notación de la práctica anterior. Por ejemplo,
-        method = ('SGD', {'eta': 0.1}) describirá un descenso de gradiente estocástico
-        con learning rate = 0.1
-        '''
         def SGD_adapter(params, name):
             return tf.train.GradientDescentOptimizer(
                 learning_rate=params['eta'],
@@ -196,8 +188,8 @@ def NetConstructor(object):
                            
         with tf.name_scope('loss'):
             method_class = optimizers_dict[method[0]]
-            loss_fn = self.loss_dict[self.layers[-1]['activation']]
-            self.loss = tf.reduce_mean(loss_fn(logits=self.y,
+            loss_fn = self.loss_dict[self.last_activation]
+            self.loss = tf.reduce_mean(loss_fn(logits=self.logits,
                                               labels=self.y_),
                                        name='loss')
             optimizer = method_class(method[1], name='optimizer')
@@ -232,14 +224,9 @@ def NetConstructor(object):
         
 
     def predict(self, x_test):
-        '''
-        El método predict recibirá datos de test y devolverá 
-        la predicción de la red, una vez entrenada.
-        '''
         with tf.Session() as sess:
             self.saver.restore(sess, "./MLP.ckpt")
-            loss_fn = self.loss_dict[self.layers[-1]['activation']]
-            pred = loss_fn(self.y)
+            pred = self.last_activation(self.logits)
             y_pred = sess.run(pred, feed_dict={self.x: x_test})
         return y_pred
 
@@ -253,9 +240,9 @@ if __name__ == "__main__":
     x_data = np.vstack((x_data_black, x_data_red))
     t_data = np.asarray([0]*nb_black + [1]*nb_red).reshape(nb_data, 1)
     
-    layer_0 = {'dim': 30}
-    layer_1 = {'type': 'fc', 'dim': 50, 'activation': 'sigmoid'}
-    layer_2 = {'type': 'fc', 'dim': 10, 'activation': 'sigmoid'}
+    layer_0 = {'dim': 2}
+    layer_1 = {'type': 'fc', 'dim': 50, 'activation': 'sigmoid', 'init': 'he'}
+    layer_2 = {'type': 'fc', 'dim': 1, 'activation': 'sigmoid', 'init': 'xavier'}
     layer_list = [layer_0, layer_1, layer_2]
     net = NetConstructor(layer_list)
     net.train(x_data, t_data)
